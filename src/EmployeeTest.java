b575b6de0646cca9bce0d8399369f50d0ae91ffa
tirth2871 <t5tirth@gmail.com>
2023-02-15 13:29:47 +0530

    Adding new file EmployeeTest.java

---
+++ src/EmployeeTest.java
@@ -0,0 +1,12 @@
+import java.time.*;
+
+public class EmployeeTest {
+    public static void  main(String[] args) {
+
+        // fill the staff array with three Employee objects
+        Employee[] staff= new Employee[3];
+
+        staff[0] = new Employee("Tirth Shah", 75000, 2001, 12, 15);
+
+    }
+}
