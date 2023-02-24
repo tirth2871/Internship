4a867c7dd742ffa68c9fb884dee7aa407a352fc0
tirth2871 <t5tirth@gmail.com>
2023-02-24 16:50:07 +0530

    Adding Employee Manager & ManagerTest in package inheritance

---
+++ src/abstractClasses/PersonTest.java
@@ -0,0 +1,17 @@
+package abstractClasses;
+
+import inheritance.Employee;
+
+public class PersonTest {
+    public static void main(String[] args) {
+        Person[] people = new Person[2];
+
+        // fill the people array with Student and Employee objects
+        people[0] = new Employee("Harry Hacker", 50000, 1989, 10, 1);
+        people[1] = new Student("Maria Morris", "Computer Science");
+
+        // print out names and descriptions of all Person ojects
+        for (Person p: people)
+            System.out.println(p.getName() + ", " + p.getDescription());
+    }
+}
