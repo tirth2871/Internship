1d24c402091c02f74e2ee8a661cb3b547c9bc439
tirth2871 <t5tirth@gmail.com>
2023-03-09 14:46:16 +0530

    completed Employee.java in clone package

--- src/clone/Employee.java
+++ src/clone/Employee.java
@@ -1,4 +1,46 @@
 package clone;
 
-public class Employee {
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Employee implements Cloneable {
+    private String name;
+    private double salary;
+    public Date hireDay;
+
+    public Employee(String name, double salary) {
+
+        this.name = name;
+        this.salary = salary;
+        hireDay = new Date();
+    }
+
+    public Employee clone() {
+
+        // call Object.clone();
+        Employee cloned = (Employee) super.clone();
+
+        // clone mutable fields
+        cloned.hireDay = (Date) hireDay.clone();
+
+        return cloned;
+    }
+
+    public void setHireDay(int year, int month, int day) {
+
+        Date newHireDay = new GregorianCalendar(year, month - 1, day).getTime();
+
+        // Example of instance field mutation
+        hireDay.setTime(newHireDay.getTime());
+    }
+
+    public void raiseSalary(double byPercent) {
+
+        double raise = salary * byPercent / 100;
+        salary += raise;
+    }
+
+    public String toString() {
+        return "Employee[Name = " + name + "Salary = " + salary + " HireDay = " + hireDay + "]"
+    }
 }
